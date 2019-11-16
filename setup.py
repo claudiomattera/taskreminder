76111fc0a878024a2b904b10af36ae4de960ba87
Claudio Mattera <claudio@mattera.it>
2019-11-16 21:06:48 +0100

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name="task-reminder",
-    version="0.1.0",
+    version="0.1.1",
     description="Sends reminders for tasks from Taskwarrior",
     long_description=open("Readme.md").read(),
     author="Claudio Giovanni Mattera",
